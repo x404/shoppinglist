1aaccff075224c9d3abf0c3bbba63afc48b9c9c5
x404 <kronos2003@gmail.com>
2025-03-31 17:46:52 +0300

    feat: created CategoryItem component

---
+++ src/components/CategoryItem/CategoryItem.tsx
@@ -0,0 +1,32 @@
+import styles from './Category.module.css';
+
+interface CategoryItem {
+    category: string;
+    count: number;
+    isActive: boolean;
+    onSelectCategory: (event: React.MouseEvent<HTMLAnchorElement>, category: string) => void;
+    allCategory: string;
+}
+
+function CategoryItem({ category, count, isActive, onSelectCategory, allCategory }: CategoryItem) {
+    return (
+        <li className={`${styles['menu-item']} d-flex align-items-center mt-1 ps-2 pe-2 ${isActive ? styles.active : ''}`}>
+            <a href={`#${category}`}
+               className={`${styles['sidebar-link']} flex-grow-1 ps-2 ${category === allCategory ? 'fw-bold text-uppercase' : ''}`}
+               {...(isActive ? { 'aria-current': 'page' } : {})}
+               title=""
+               onClick={(event) => onSelectCategory(event, category)}
+            >
+                {category}
+            </a>
+            <div
+                className={`${styles.counter} d-flex align-items-center justify-content-center p-1`}
+                aria-label={`${count} items`}
+            >
+                {count}
+            </div>
+        </li>
+    );
+}
+
+export default CategoryItem;
--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,8 +1,8 @@
-import styles from './Sidebar.module.css';
 import { useSelector } from 'react-redux';
 import { selectProductItems } from '../../store/productListSlice';
 import { Product } from "../../types/types";
 import { useMemo, useState } from "react";
+import CategoryItem from "../CategoryItem/CategoryItem";
 
 const getNamesCategories = (productList: Product[]): string[] => {
     const categories = new Set(productList.map(product => product.category));
@@ -39,33 +39,16 @@ const Sidebar = () => {
                 <nav>
                     <h2 className="visually-hidden">Main menu</h2>
                     <ul className="list-unstyled menu">
-                        {categories.map((category) => {
-                            const isActiveCategory = selectedCategory === category;
-                            const count = getCategoryCount(category);
-
-                            return (
-                                <li
-                                    className={`${styles['menu-item']} d-flex align-items-center mt-1 ps-2 pe-2 ${isActiveCategory ? `${styles.active}` : ''}`}
-                                    key={category}
-                                >
-                                    {/*TODO: Router Link*/}
-                                    <a href={`#${category}`}
-                                       className={`${styles['sidebar-link']} flex-grow-1 ps-2 ${category === allCategory ? 'fw-bold text-uppercase' : ''}`}
-                                       {...(isActiveCategory ? { 'aria-current': 'page' } : {})}
-                                       title=""
-                                       onClick={(event) => onSelectCategory(event, category)}
-                                    >
-                                        {category}
-                                    </a>
-                                    <div
-                                        className={`${styles.counter} d-flex align-items-center justify-content-center p-1`}
-                                        aria-label={`${count} items`}
-                                    >
-                                        {count}
-                                    </div>
-                                </li>
-                            )
-                        })}
+                        {categories.map((category) => (
+                            <CategoryItem
+                                key={category}
+                                category={category}
+                                count={getCategoryCount(category)}
+                                isActive={selectedCategory === category}
+                                onSelectCategory={onSelectCategory}
+                                allCategory={allCategory}
+                            />
+                        ))}
                     </ul>
                 </nav>
             </aside>
