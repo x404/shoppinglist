fe8d0bf0b78e5dc6b0448a6714d2b32254c8e880
x404 <kronos2003@gmail.com>
2025-04-02 11:32:07 +0300

    feat: stylized 'edit', 'delete' buttons

--- src/components/ProductItem/ProductItem.tsx
+++ src/components/ProductItem/ProductItem.tsx
@@ -43,7 +43,7 @@ const ProductItem = ({ product }: { product: Product }) => {
                         </div>
                     </div>
 
-                    <div className="actions d-flex ms-3">
+                    <div className="actions d-flex ms-3 gap-1">
                         <Button
                             variant="outline-dark"
                             size="sm"
@@ -52,27 +52,29 @@ const ProductItem = ({ product }: { product: Product }) => {
                             data-tooltip-content="Edit item"
                             data-tooltip-place="top"
                             onClick={onEditProduct}
-                            className="bg-white"
+                            className={styles.editButton}
                         >
-                            <svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" width="16" height="16"
-                                 fill="currentColor" className="bi bi-pen" viewBox="0 0 16 16">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                                 className="bi bi-pencil" viewBox="0 0 16 16">
                                 <path
-                                    d="m13.498.795.149-.149a1.207 1.207 0 1 1 1.707 1.708l-.149.148a1.5 1.5 0 0 1-.059 2.059L4.854 14.854a.5.5 0 0 1-.233.131l-4 1a.5.5 0 0 1-.606-.606l1-4a.5.5 0 0 1 .131-.232l9.642-9.642a.5.5 0 0 0-.642.056L6.854 4.854a.5.5 0 1 1-.708-.708L9.44.854A1.5 1.5 0 0 1 11.5.796a1.5 1.5 0 0 1 1.998-.001"/>
+                                    d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325"/>
                             </svg>
                         </Button>
                         <Button variant="outline-dark"
                                 size="sm"
-                                className={`${styles['delete-btn']} bg-white ms-1`}
+                                className={styles.deleteButton}
                                 aria-label={`Delete ${product.name}`}
                                 data-tooltip-id="delete-tooltip"
                                 data-tooltip-content="Delete item"
                                 data-tooltip-place="top"
                                 onClick={onDeleteProduct}
                         >
-                            <svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" width="16" height="16"
-                                 fill="currentColor" className="bi bi-trash3-fill" viewBox="0 0 16 16">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                                 className="bi bi-trash" viewBox="0 0 16 16">
                                 <path
-                                    d="M11 1.5v1h3.5a.5.5 0 0 1 0 1h-.538l-.853 10.66A2 2 0 0 1 11.115 16h-6.23a2 2 0 0 1-1.994-1.84L2.038 3.5H1.5a.5.5 0 0 1 0-1H5v-1A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5"/>
+                                    d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z"/>
+                                <path
+                                    d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z"/>
                             </svg>
                         </Button>
                     </div>
