2e4e49085fa2374ebf89b903d3021669c2dcbcc3
x404 <kronos2003@gmail.com>
2025-04-03 17:49:24 +0300

    refactor: added PlusIcon

--- src/components/CategoryHeader/CategoryHeader.tsx
+++ src/components/CategoryHeader/CategoryHeader.tsx
@@ -1,43 +1,50 @@
-import { memo } from "react";
 import { Badge, Button } from "react-bootstrap";
 
 // constants
 import { ALL_CATEGORY_NAME } from "../../constants/categories";
 
 // interfaces
-interface CategoryHeader {
+interface CategoryHeaderProp {
     category: string;
     counter: number;
     activeCategory: string;
     onAddProduct: (category: string) => void;
 }
 
+const PlusIcon = () => {
+    return (
+        <>
+            <svg xmlns="http://www.w3.org/2000/svg"
+                 aria-hidden="true"
+                 width="16"
+                 height="16"
+                 fill="currentColor"
+                 className="bi bi-plus"
+                 viewBox="0 0 16 16"
+            >
+                <path
+                    d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4"/>
+            </svg>
+        </>
+    )
+}
 
-export const CategoryHeader = memo(({ category, counter, activeCategory, onAddProduct }: CategoryHeader) => {
-        const isAllCategory = activeCategory === ALL_CATEGORY_NAME;
-        return (
-            <h4 className={`d-flex align-items-center gap-2 ${isAllCategory ? 'h6 text-uppercase' : 'h5 mb-4 fw-normal'}`}>
-                <div className={`${isAllCategory ? 'fw-bold' : ''}`}>{category}</div>
-                <Badge bg="secondary">{counter}</Badge>
-                <Button
-                    variant="light"
-                    size="sm"
-                    onClick={() => onAddProduct(category)}
-                >
-                    <svg xmlns="http://www.w3.org/2000/svg"
-                         aria-hidden="true"
-                         width="16"
-                         height="16"
-                         fill="currentColor"
-                         className="bi bi-plus"
-                         viewBox="0 0 16 16"
-                    >
-                        <path
-                            d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4"/>
-                    </svg>
-                    Add product
-                </Button>
-            </h4>
-        )
-    }
-);
+
+export const CategoryHeader = ({ category, counter, activeCategory, onAddProduct }: CategoryHeaderProp) => {
+    const isAllCategory = activeCategory === ALL_CATEGORY_NAME;
+
+    return (
+        <h4 className={`d-flex align-items-center gap-2 ${isAllCategory ? 'h6 text-uppercase' : 'h5 mb-4 fw-normal'}`}>
+            <div className={`${isAllCategory ? 'fw-bold' : ''}`}>{category}</div>
+            <Badge bg="secondary">{counter}</Badge>
+            <Button
+                variant="light"
+                size="sm"
+                onClick={() => onAddProduct(category)}
+            >
+                <PlusIcon/>
+                Add product
+            </Button>
+        </h4>
+    )
+};
