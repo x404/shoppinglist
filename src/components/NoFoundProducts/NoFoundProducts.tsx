f25a31dd0a1fcc87c057ffbc73af6224de9518b1
x404 <kronos2003@gmail.com>
2025-04-03 15:47:58 +0300

    fixes: fix missing select when all products are remove

--- src/components/NoFoundProducts/NoFoundProducts.tsx
+++ src/components/NoFoundProducts/NoFoundProducts.tsx
@@ -9,15 +9,16 @@ import { Product } from "../../types/types";
 interface NoFoundProductsProps {
     products: Product[];
     activeCategory: string;
-    onAddProduct: (activeCategory: string) => void;
+    onAddProduct: (activeCategory: string | null) => void;
 }
 
 
 const NoFoundProducts = ({ products, activeCategory, onAddProduct }: NoFoundProductsProps) => {
     if (products.length > 0) return;
     
-    const onEmitAddProduct = () => {
-        onAddProduct(activeCategory);    
+    const handleAddProduct = () => {
+        const category = activeCategory !== 'All' ? activeCategory : null;
+        onAddProduct(category);
     }
     
     return (
@@ -30,7 +31,7 @@ const NoFoundProducts = ({ products, activeCategory, onAddProduct }: NoFoundProd
                     onAddProduct={onAddProduct}
                 />
                 <p>No products found</p>
-                <Button variant="dark" onClick={onEmitAddProduct}>Add first product</Button>
+                <Button variant="dark" onClick={handleAddProduct}>Add first product</Button>
             </article>
         </>
     );
