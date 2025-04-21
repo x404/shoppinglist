b01083ae49628ba530fde0367f0d52c312187171
x404 <kronos2003@gmail.com>
2025-04-21 14:37:30 +0300

    fix: fix title styles for responsive

--- src/store.ts
+++ src/store.ts
@@ -1,6 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
-import productListReducer from "./store/productListSlice";
-import categoriesReducer from "./store/categoriesSlice";
+import productListReducer from "@store/productListSlice";
+import categoriesReducer from "@store/categoriesSlice";
 
 const store = configureStore({
     reducer: {
