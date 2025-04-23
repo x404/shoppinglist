42a22a60317ce4a69ceabd3e6cb9f8c6e9a3bbea
x404 <kronos2003@gmail.com>
2025-04-23 17:01:31 +0300

    feat: sepatated categoryItems  dropdown component

---
+++ src/components/CategoryActionsDropdown/CategoryActionsDropdown.tsx
@@ -0,0 +1,106 @@
+import { Dropdown } from "react-bootstrap";
+import {
+    ChevronRight,
+    FileEarmarkPlus,
+    FolderPlus, Palette,
+    Pencil,
+    Plus,
+    Shuffle,
+    ThreeDots, Trash,
+    XOctagon
+} from "react-bootstrap-icons";
+
+import ActionItem from "./ActionItem/ActionItem";
+
+
+import styles from './CategoryActionsDropdown.module.css';
+import sidebarStyles from './../Sidebar/Sidebar.module.css';
+
+interface CategoryActionsDropdownProps {
+    onOpenAddProductModal: () => void;
+    isVisible: boolean;
+}
+
+
+const CategoryActionsDropdown = ({ onOpenAddProductModal, isVisible }: CategoryActionsDropdownProps) => {
+    return (
+        <div
+            className={`${sidebarStyles.actions} ${styles.actions} ${isVisible ? styles.visible : ""} d-flex align-items-center position-absolute me-2 end-0`}>
+            <Dropdown drop="end">
+                <Dropdown.Toggle
+                    as="button"
+                    className={`${sidebarStyles.settingsCategoryBtn} ${sidebarStyles.noRightArrow} btn d-flex align-items-center justify-content-center px-1`}
+                    size="sm"
+                    variant="light"
+                    data-tooltip-id="sidebar-tooltip"
+                    data-tooltip-content="Create category, add product, etc."
+                    data-tooltip-place="top"
+                >
+                    <ThreeDots size={12}/>
+                </Dropdown.Toggle>
+
+                <Dropdown.Menu>
+                    <ActionItem icon={<Pencil size={16} />} label="Rename" disabled onClick={() => {}} />
+                    <ActionItem icon={<XOctagon size={16} />} label="Clear" disabled onClick={() => {}} />
+                    <Dropdown.Divider/>
+
+
+                    <Dropdown drop="end">
+                        <Dropdown.Toggle
+                            as="button"
+                            className={`${sidebarStyles.noRightArrow} dropdown-item btn d-flex align-items-center justify-content-center px-1`}
+                            size="sm"
+                            variant="light"
+                        >
+                            <div className={`${styles.icon} mx-1`}>
+                                <Plus size={22}/>
+                            </div>
+
+
+                            <div className={`${styles.title} flex-grow-1`}>
+                                Create new
+                            </div>
+
+                            <ChevronRight size={12} className="ms-2 me-2"/>
+                        </Dropdown.Toggle>
+
+                        <Dropdown.Menu>
+                            <ActionItem icon={<FolderPlus size={16} />} label="Category" disabled onClick={() => {}} />
+                            <ActionItem icon={<FileEarmarkPlus size={16} />} label="Product" onClick={onOpenAddProductModal} />
+                        </Dropdown.Menu>
+                    </Dropdown>
+
+
+                    <ActionItem icon={<Shuffle size={16} />} label="Move to" disabled onClick={() => {}} />
+                    <ActionItem icon={<Palette size={16} />} label="Color & Icon" disabled onClick={() => {}} />
+                    
+                    <Dropdown.Divider/>
+                    <ActionItem icon={<Trash size={16} />} label="Delete" disabled onClick={() => {}} className={'text-danger'} />
+                </Dropdown.Menu>
+            </Dropdown>
+
+
+            <Dropdown drop="end">
+                <Dropdown.Toggle
+                    as="button"
+                    className={` ${sidebarStyles.noRightArrow} btn d-flex align-items-center p-0 border-0`}
+                    size="sm"
+                    variant=""
+                    data-tooltip-id="sidebar-tooltip"
+                    data-tooltip-content="Add product"
+                    data-tooltip-place="top"
+                >
+                    <Plus size={20}/>
+                </Dropdown.Toggle>
+
+                <Dropdown.Menu>
+                    <ActionItem icon={<FileEarmarkPlus size={16} />} label="Product" onClick={() => onOpenAddProductModal()} />
+                    {/*<Dropdown.Divider/>*/}
+                    {/*<Dropdown.Item as="button">Something else</Dropdown.Item>*/}
+                </Dropdown.Menu>
+            </Dropdown>
+        </div>
+    )
+}
+
+export default CategoryActionsDropdown;
--- src/components/CategoryItem/CategoryItem.tsx
+++ src/components/CategoryItem/CategoryItem.tsx
@@ -1,25 +1,16 @@
-import { MouseEvent } from "react";
+import { MouseEvent, useState } from "react";
 
-// components
-import { Dropdown } from "react-bootstrap";
-import {
-    ChevronRight, FileEarmarkPlus, FolderPlus,
-    Palette,
-    Pencil,
-    Plus,
-    Shuffle,
-    ThreeDots,
-    Trash, XOctagon
-} from "react-bootstrap-icons";
+// redux, context
+import { useModal } from "@context/ModalContext";
+
 
 // styles
 import styles from './CategoryItem.module.css';
 import sidebarStyles from './../Sidebar/Sidebar.module.css';
-import { useModal } from "../../context/ModalContext";
-import { Category } from "../../types/types";
-
 
 // interfaces
+import { Category } from "@/types/types";
+import CategoryActionsDropdown from "../CategoryActionsDropdown/CategoryActionsDropdown";
 interface CategoryItem {
     category: Category;
     count: number;
@@ -37,14 +28,20 @@ const CategoryItem = ({ category, count, isActive, onSelectCategory, allCategory
     const allCategoryHighlightClass = name === allCategory ? 'fw-bold text-uppercase' : '';
     const allCategoryClass = name === allCategory ? styles.menuAllItem : '';
 
+    const [isHovered, setIsHovered] = useState(false);
 
-    const onAddProduct = () => {
+
+    const onOpenAddProductModal = () => {
         openAddProductModal(name);
     }
 
     return (
         <>
-            <li className={`${styles.menuItem} ${allCategoryClass} d-flex align-items-center mt-1 px-2 position-relative ${activeClass}`}>
+            <li 
+                className={`${styles.menuItem} ${allCategoryClass} d-flex align-items-center mt-1 px-2 position-relative ${activeClass}`}
+                onMouseEnter={() => setIsHovered(true)}
+                onMouseLeave={() => setIsHovered(false)}
+            >
                 <a href={`#${id}`}
                    className={`${styles.sidebarLink} flex-grow-1 ps-2 ${allCategoryHighlightClass}`}
                    {...(isActive ? { 'aria-current': 'page' } : {})}
@@ -60,170 +57,10 @@ const CategoryItem = ({ category, count, isActive, onSelectCategory, allCategory
                     {count}
                 </div>
                 {name !== allCategory && (
-                    <div
-                        className={`${sidebarStyles.actions} ${styles.actions} d-flex align-items-center position-absolute me-2 end-0`}>
-                        <Dropdown drop="end">
-                            <Dropdown.Toggle
-                                as="button"
-                                className={`${sidebarStyles.settingsCategoryBtn} ${sidebarStyles.noRightArrow} btn d-flex align-items-center justify-content-center px-1`}
-                                size="sm"
-                                variant="light"
-                                data-tooltip-id="sidebar-tooltip"
-                                data-tooltip-content="Create category, add product, etc."
-                                data-tooltip-place="top"
-                            >
-                                <ThreeDots size={12}/>
-                            </Dropdown.Toggle>
-
-                            <Dropdown.Menu>
-                                <Dropdown.Item as="button" className="ps-2 disabled">
-                                    <div className="d-flex">
-                                        <div className={`${styles.icon} me-2`}>
-                                            <Pencil size={16}/>
-                                        </div>
-                                        <div className={`${styles.title}`}>
-                                            Rename
-                                        </div>
-                                    </div>
-                                </Dropdown.Item>
-
-                                <Dropdown.Item
-                                    as="button"
-                                    className="ps-2  disabled"
-                                >
-                                    <div className="d-flex">
-                                        <div className={`${styles.icon} me-2`}>
-                                            <XOctagon size={16}/>
-                                        </div>
-                                        <div className={`${styles.title} flex-grow-1`}>
-                                            Clear
-                                        </div>
-                                    </div>
-                                </Dropdown.Item>
-
-
-                                <Dropdown.Divider/>
-
-
-                                <Dropdown drop="end">
-                                    <Dropdown.Toggle
-                                        as="button"
-                                        className={`${sidebarStyles.noRightArrow} dropdown-item btn d-flex align-items-center justify-content-center px-1`}
-                                        size="sm"
-                                        variant="light"
-                                    >
-                                        <div className={`${styles.icon} mx-1`}>
-                                            <Plus size={22}/>
-                                        </div>
-
-
-                                        <div className={`${styles.title} flex-grow-1`}>
-                                            Create new
-                                        </div>
-
-                                        <ChevronRight size={12} className="ms-2 me-2"/>
-                                    </Dropdown.Toggle>
-
-                                    <Dropdown.Menu>
-                                        <Dropdown.Item as="button" className="ps-2 disabled">
-                                            <div className="d-flex">
-                                                <div className={`${styles.icon} me-2`}>
-                                                    <FolderPlus size={16}/>
-                                                </div>
-                                                <div className="title flex-grow-1">
-                                                    Category
-                                                </div>
-                                            </div>
-                                        </Dropdown.Item>
-                                        <Dropdown.Item as="button" className="ps-2" onClick={onAddProduct}>
-                                            <div className="d-flex">
-                                                <div className={`${styles.icon} me-2`}>
-                                                    <FileEarmarkPlus size={16}/>
-                                                </div>
-                                                <div className="title flex-grow-1">
-                                                    Product
-                                                </div>
-                                            </div>
-                                        </Dropdown.Item>
-                                    </Dropdown.Menu>
-                                </Dropdown>
-
-
-                                <Dropdown.Item
-                                    as="button"
-                                    className="px-1 disabled w-100"
-                                >
-                                    <div className="d-flex">
-                                        <div className={`${styles.icon} mx-1`}>
-                                            <Shuffle size={16}/>
-                                        </div>
-                                        <div className={`${styles.title} flex-grow-1`}>
-                                            Move to
-                                        </div>
-                                    </div>
-                                </Dropdown.Item>
-
-                                <Dropdown.Item
-                                    as="button"
-                                    className="px-1 disabled"
-                                >
-                                    <div className="d-flex">
-                                        <div className={`${styles.icon} mx-1`}>
-                                            <Palette size={16}/>
-                                        </div>
-                                        <div className={`${styles.title} flex-grow-1`}>
-                                            Color & Icon
-                                        </div>
-                                    </div>
-                                </Dropdown.Item>
-                                <Dropdown.Divider/>
-                                <Dropdown.Item
-                                    as="button"
-                                    className="ps-2 text-danger disabled"
-                                >
-                                    <div className="d-flex">
-                                        <div className={`${styles.icon} me-2`}>
-                                            <Trash size={16}/>
-                                        </div>
-                                        <div className={`${styles.title} flex-grow-1`}>
-                                            Delete
-                                        </div>
-                                    </div>
-                                </Dropdown.Item>
-
-                            </Dropdown.Menu>
-                        </Dropdown>
-
-
-                        <Dropdown drop="end">
-                            <Dropdown.Toggle
-                                as="button"
-                                className={` ${sidebarStyles.noRightArrow} btn d-flex align-items-center p-0 border-0`}
-                                size="sm"
-                                variant=""
-                                data-tooltip-id="sidebar-tooltip"
-                                data-tooltip-content="Add product"
-                                data-tooltip-place="top"
-                            >
-                                <Plus size={20}/>
-                            </Dropdown.Toggle>
-
-                            <Dropdown.Menu>
-                                <Dropdown.Item as="button" className="" onClick={onAddProduct}>
-                                    <div className="d-flex">
-                                        <div className={`${sidebarStyles.noRightArrow} me-2`}>
-                                            <FileEarmarkPlus size={16}/>
-                                        </div>
-                                        <div className="title flex-grow-1">
-                                            Product
-                                        </div>
-                                    </div>
-                                </Dropdown.Item>
-                                {/*<Dropdown.Divider/>*/}
-                                {/*<Dropdown.Item as="button">Something else</Dropdown.Item>*/}
-                            </Dropdown.Menu>
-                        </Dropdown>
-                    </div>
+                    <CategoryActionsDropdown 
+                        onOpenAddProductModal={onOpenAddProductModal}
+                        isVisible={isHovered}
+                    />
                 )}
             </li>
         </>
